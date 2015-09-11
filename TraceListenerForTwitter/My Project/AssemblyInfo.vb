3ec84f28e0a92d5e7a3af206b341777a31c2a56c
jz5 <jz5@hotmail.com>
2015-09-11 12:33:21 +0900

    Change TargetFramework (4.5.2 -> 4.5)

--- TraceListenerForTwitter/My Project/AssemblyInfo.vb
+++ TraceListenerForTwitter/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("")>
-<Assembly: AssemblyCopyright("Copyright ©  jz5 2015")>
+<Assembly: AssemblyCopyright("Copyright ©  2015 jz5")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.1.0")>
-<Assembly: AssemblyFileVersion("1.0.1.0")>
+<Assembly: AssemblyVersion("1.0.2.0")>
+<Assembly: AssemblyFileVersion("1.0.2.0")>
